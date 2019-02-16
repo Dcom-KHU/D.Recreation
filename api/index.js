cadc97fc4bfa8832eea240c0f1095da2ec87bb2a
박민재 <sobu0715@gmail.com>
2019-02-17 03:17:42 +0900

    feat: Router Setting

--- api/index.js
+++ api/index.js
@@ -2,7 +2,17 @@
 
 const express = require('express')
 const router = express.Router()
+const diceroll = require('./diceroll')
+const draw = require('./draw')
+const scoreboard = require('./scoreboard')
+const speedquiz = require('./speedquiz')
+const worldcup = require('./worldcup')
 
+router.use('/diceroll', diceroll)
+router.use('/draw', draw)
+router.use('/scoreboard', scoreboard)
+router.use('/speedquiz', speedquiz)
+router.use('/worldcup', worldcup)
 router.get('/test', (req, res)=>{return res.status(200).json({test:true})})
 
 module.exports = router
