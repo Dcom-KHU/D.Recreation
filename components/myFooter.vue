27f59afbead77dfa635672f65e65081eb9f10482
박민재 <sobu0715@gmail.com>
2019-02-18 04:01:18 +0900

    feat: Frontend Main Page #1

---
+++ components/myFooter.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="footer">
+            <div class="title">D.Recreation</div>
+            <div class="made">Made By WYDGA</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "myFooter"
+    }
+</script>
+
+<style scoped>
+    div.footer{
+        border-top: #7f828b 1px solid;
+        color: #47494e;
+        padding: 10px;
+    }
+    @media screen and (max-width: 890px) {
+        div.footer>div {
+            padding-left: 10px;
+        }
+    }
+    @media screen and (min-width: 891px) {
+        div.footer>div {
+            padding-left: 30px;
+        }
+    }
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,20 @@
 module.exports = {
     head: {
-        titleTemplate: '%s - JUSTKODE',
+        titleTemplate: '%s - D.Recreation',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1'},
-            { hid: 'description', name: 'description', content: 'JustKode의 Nuxt.js 연습'}
+            { hid: 'description', name: 'description', content: 'D.Recreation 홈페이지 입니다.'},
+        ]
+    },
+    css: [
+        'assets/main.css'
+    ],
+    build: {
+        vendor: [
+            '@fortawesome/fontawesome',
+            '@fortawesome/fontawesome-free-solid',
+            '@fortawesome/fontawesome-free-brands'
         ]
     }
 }
